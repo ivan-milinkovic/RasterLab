1a61e98303c865f57d1af6ed4158567542ce9727
Ivan Milinkovic <ivanmcode@gmail.com>
2023-07-15 21:14:21 +0200

    Initial Commit

---
+++ RasterLab/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  RasterLab
+//
+//  Created by Ivan Milinkovic on 15.7.23..
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
